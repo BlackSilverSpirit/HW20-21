ec93f88e51966cf87f69d35e7c0d2e139d302a5b
Евгений Крутиков <50365848+BlackSilverSpirit@users.noreply.github.com>
2025-04-25 21:04:15 +0700

    1

--- Assets/HM20-21/scripts/Drag.cs
+++ Assets/HM20-21/scripts/Drag.cs
@@ -13,13 +13,11 @@ namespace HW2021
 
             var ray = cam.ScreenPointToRay(Input.mousePosition);
 
-            var dragPlane = new Plane(Vector3.up, Vector3.zero);
-
-            if (dragPlane.Raycast(ray, out float distance))
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Vector3 hitPoint = ray.GetPoint(distance);
+                Vector3 surfacePoint = hit.point;
 
-                Vector3 targetPosition = new Vector3(hitPoint.x, _dragHeight, hitPoint.z);
+                Vector3 targetPosition = new Vector3(surfacePoint.x, hit.point.y + _dragHeight, surfacePoint.z);
 
                 selectedObject.MovePosition(Vector3.Lerp(selectedObject.position, targetPosition,
                     Time.deltaTime * dragSpeed));
